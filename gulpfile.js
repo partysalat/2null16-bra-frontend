c55cb559dcbfd80ae8640c9fbb36304d987330e1
Ben Lenser <ben.lenser@immobilienscout24.de>
2017-04-23 19:51:34 +0200

    add ava as test runner

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,23 @@
 var gulp = require('gulp');
-require("require-dir")("./gulp/tasks");
-var sequence = require("run-sequence");
+require('require-dir')('./gulp/tasks');
+var sequence = require('run-sequence');
 
-gulp.task("default", function (done) {
+gulp.task('default', function (done) {
   sequence(
     '_clean',
     '_scripts:watch',
-    ['_unit:browser:watch',"_server",'_watch'],
+    ['_server','_watch'],
     done
   );
 });
 
-gulp.task("build",["_scripts"]);
+gulp.task('build',['_scripts']);
 
-gulp.task("start", function (done) {
+gulp.task('start', function (done) {
   sequence(
     '_clean',
     '_scripts',
-    "_server",
+    '_server',
     done
   );
 });
---
+++ src/server/index.spec.js
@@ -0,0 +1,8 @@
+import { describe} from 'ava-spec';
+
+describe('indexHandler.dutrinkst', (it) => {
+
+  it('calls templateService.renderHtml', (t) => {
+    t.truthy(true);
+  });
+});
